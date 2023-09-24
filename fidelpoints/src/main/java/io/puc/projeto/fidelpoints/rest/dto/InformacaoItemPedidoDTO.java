41bc6a5586599777c6fa1ff2ebb85ef929e97515
Stephanie Ingrid <stephanieingrid5@gmail.com>
2023-09-24 15:35:15 -0300

    Informação Item Pedido DTO

--- fidelpoints/src/main/java/io/puc/projeto/fidelpoints/rest/dto/InformacaoItemPedidoDTO.java
+++ fidelpoints/src/main/java/io/puc/projeto/fidelpoints/rest/dto/InformacaoItemPedidoDTO.java
@@ -1,11 +1,9 @@
-package io.projeto.backend.fidelpoints.rest.dto;
+package io.puc.projeto.fidelpoints.rest.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.math.BigDecimal;
 
 @Data
 @AllArgsConstructor
