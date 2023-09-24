0be9028f9dc7a04003d3963ab6f828ce2558c582
Stephanie Ingrid <stephanieingrid5@gmail.com>
2023-09-24 15:18:10 -0300

    Items pedido Repository

--- fidelpoints/src/main/java/io/puc/projeto/fidelpoints/domain/repository/ItemsPedidoRepository.java
+++ fidelpoints/src/main/java/io/puc/projeto/fidelpoints/domain/repository/ItemsPedidoRepository.java
@@ -1,6 +1,6 @@
-package io.projeto.backend.fidelpoints.domain.repository;
+package io.puc.projeto.fidelpoints.domain.repository;
 
-import io.stephanie.backend.fidelpoints.domain.entity.ItemPedido;
+import io.puc.projeto.fidelpoints.domain.entity.ItemPedido;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ItemsPedidoRepository extends JpaRepository<ItemPedido, Integer>{
