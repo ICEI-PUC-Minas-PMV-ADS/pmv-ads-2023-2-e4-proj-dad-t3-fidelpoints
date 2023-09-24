7c9d8bdd2cb4fd520bfa7d01ef8c9ca897be3159
Stephanie Ingrid <stephanieingrid5@gmail.com>
2023-09-24 15:20:42 -0300

    Produtos Repository

--- fidelpoints/src/main/java/io/puc/projeto/fidelpoints/domain/repository/ProdutosRepository.java
+++ fidelpoints/src/main/java/io/puc/projeto/fidelpoints/domain/repository/ProdutosRepository.java
@@ -1,6 +1,6 @@
-package io.projeto.backend.fidelpoints.domain.repository;
+package io.puc.projeto.fidelpoints.domain.repository;
 
-import io.stephanie.backend.fidelpoints.domain.entity.Produto;
+import io.puc.projeto.fidelpoints.domain.entity.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProdutosRepository extends JpaRepository <Produto, Integer>{
