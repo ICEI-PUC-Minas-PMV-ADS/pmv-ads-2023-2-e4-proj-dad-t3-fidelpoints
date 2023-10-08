d0d97ca352d7c22873da74f95891b4e939feb573
renato_silva <62575524+renato-silva1990@users.noreply.github.com>
2023-10-08 19:47:00 -0300

    Delete fidelpoints directory

--- fidelpoints/src/main/java/io/puc/projeto/fidelpoints/domain/repository/ProdutosRepository.java
+++
@@ -1,8 +0,0 @@
-package io.puc.projeto.fidelpoints.domain.repository;
-
-import io.puc.projeto.fidelpoints.domain.entity.Produto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProdutosRepository extends JpaRepository <Produto, Integer>{
-
-}
