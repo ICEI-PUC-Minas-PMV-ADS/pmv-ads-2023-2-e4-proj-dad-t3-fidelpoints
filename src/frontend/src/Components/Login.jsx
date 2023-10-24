108d775aa43faf220f0507285cf0b78cc9d08170
Stephanie Ingrid <stephanieingrid5@gmail.com>
2023-10-24 19:40:18 -0300

    Componente Login

--- src/frontend/src/Components/Login.jsx
+++ src/frontend/src/Components/Login.jsx
@@ -1,8 +1,49 @@
 
-function Login(){
+import Botao from '../Components/Botao'
+import { useState } from 'react'
+import CampoTexto from '../Components/CampoTexto'
+import '../Style/Login.css'
+
+const Login = () => {
+
+
+    const[nome, setNome] = useState('')
+    const[email, setEmail] = useState('')
+
+
+
+
+    const aoSalvar = (evento) => {
+        evento.preventDefault()
+        console.log('Form foi submetido => ', nome, email, )
+    }
+
     return(
-        <h3>Testando Login!!!</h3>
+        <section className="formulario">
+            <form onSubmit={aoSalvar}>
+            <h2>Preencha os campos a baixo para se conectar</h2>
+            <CampoTexto 
+                obrigatorio = {true} 
+                label = "Nome" 
+                placeholderModificada= "Digite seu nome..."
+                valor = {nome}
+                aoAlterado = {valor => setNome (valor)}
+            />
+
+            <CampoTexto 
+                obrigatorio = {true} 
+                label = "Email" 
+                placeholderModificada= "Digite seu email..."
+                valor = {email}
+                aoAlterado = {valor => setEmail (valor)}
+                />
+
+            <Botao>
+            Conectar
+            </Botao>
+            </form>
+        </section>
     )
 }
 
-export default Login;
+export default Login
