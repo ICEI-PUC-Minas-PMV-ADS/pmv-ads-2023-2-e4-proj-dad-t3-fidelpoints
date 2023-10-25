f0ab87130c93ca3f161471fa91eabd3b82130051
Stephanie Ingrid <stephanieingrid5@gmail.com>
2023-10-25 12:43:19 -0300

    Recuperar Senha

---
+++ src/frontend/src/Components/RecuperarSenha.js
@@ -0,0 +1,39 @@
+
+import Botao from '../Components/Botao'
+import { useState } from 'react'
+import CampoTexto from '../Components/CampoTexto'
+import '../Style/RecuperarSenha.css'
+
+const RecuperarSenha = () => {
+
+
+    const[email, setEmail] = useState('')
+
+
+    const aoSalvar = (evento) => {
+        evento.preventDefault()
+        console.log('Form foi submetido => ', email, )
+    }
+
+    return(
+        <section className="formulario">
+            <form onSubmit={aoSalvar}>
+            <h2>Vamos te ajudar!</h2>
+            <p>Você receberá um link no seu e-mail cadastrado para alterar a senha</p>
+            <CampoTexto 
+                obrigatorio = {true} 
+                label = "Email" 
+                placeholderModificada= "Digite seu Email..."
+                valor = {email}
+                aoAlterado = {valor => setEmail (valor)}
+            />
+
+            <Botao>
+                Redefinir
+            </Botao>
+            </form>
+        </section>
+    )
+}
+
+export default RecuperarSenha
